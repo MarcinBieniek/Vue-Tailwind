7c2a1567ff5527a6b573878711e7720b7d7a772b
Marcin Bieniek <marcingim@gmail.com>
2023-07-15 23:32:43 +0200

    Add header component

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,3 +1,10 @@
 <template>
-  <section class="bg-customBlue">This is header</section>
+  <section class="MainHeader container  mx-auto bg-cover md:bg-contain
+  bg-no-repeat bg-left-top md:bg-center relative -top-32 md:-top-16"
+  :style="`background-image: url(${PorscheBackground}); height: calc(100vh - 128px);`">
+  </section>
 </template>
+
+<script setup>
+  import PorscheBackground from '../assets/porsche-model-header.png'
+</script>
